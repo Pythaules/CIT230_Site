ccae45b089621261d3fb4786a0f307a568068f61
Pythaules <pythaules@gmail.com>
2019-06-22 02:56:19 -0400

    L9

--- Assignments/Lesson-9/scripts/pullJSON.js
+++ Assignments/Lesson-9/scripts/pullJSON.js
@@ -1,7 +1,8 @@
 var section = document.querySelector('section');
 
+var requestURL = 'https://pythaules.github.io/CIT230_Site/Assignments/Lesson-9/towndata.json';
 var request = new XMLHttpRequest();
-request.open('GET', 'towndata.json', 'true');
+request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
@@ -11,27 +12,33 @@ request.onload = function() {
 }
 
 function populateTowns(jsonObj) {
-    var towns = jsonObj['members'];
+    var towns = jsonObj['towns'];
 
     for(var i = 0; i < towns.length; i++) {
         var myArticle = document.createElement('article');
         var myH2 = document.createElement('h2');
         var myPara1 = document.createElement('p');
+            myPara1.setAttribute("class", "motto")
         var myPara2 = document.createElement('p');
         var myPara3 = document.createElement('p');
         var myPara4 = document.createElement('p');
+        var myImg = document.createElement('img')
+            myImg.setAttribute("src", "images/" + towns[i].name + ".jpg");
+            myImg.setAttribute("alt", towns[i].name + " Image");
         
         myH2.textContent = towns[i].name;
         myPara1.textContent = 'Motto: ' + towns[i].motto;
         myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
         myPara3.textContent = 'Current Population:' + towns[i].currentPopulation;
-        myPara4.textContent = 'Annual Rainfall:';
+        myPara4.textContent = 'Annual Rainfall:' + towns[i].averageRainfall;
+        myImg.innerHTML;
    
         myArticle.appendChild(myH2);
         myArticle.appendChild(myPara1);
         myArticle.appendChild(myPara2);
         myArticle.appendChild(myPara3);
         myArticle.appendChild(myPara4);
+        myArticle.appendChild(myImg);
 
         section.appendChild(myArticle);
     }
