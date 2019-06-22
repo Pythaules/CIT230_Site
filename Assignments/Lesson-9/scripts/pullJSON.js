7b44f6dcc45a1f9eb43613900254b8efdc545fcf
Pythaules <pythaules@gmail.com>
2019-06-21 23:05:03 -0400

    L9 Index

---
+++ Assignments/Lesson-9/scripts/pullJSON.js
@@ -0,0 +1,39 @@
+var section = document.querySelector('section');
+
+var request = new XMLHttpRequest();
+request.open('GET', 'towndata.json', 'true');
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+    var showTowns = request.response;
+    populateTowns(showTowns);
+}
+
+function populateTowns(jsonObj) {
+    var towns = jsonObj['members'];
+
+    for(var i = 0; i < towns.length; i++) {
+        var myArticle = document.createElement('article');
+        var myH2 = document.createElement('h2');
+        var myPara1 = document.createElement('p');
+        var myPara2 = document.createElement('p');
+        var myPara3 = document.createElement('p');
+        var myPara4 = document.createElement('p');
+        
+        myH2.textContent = towns[i].name;
+        myPara1.textContent = 'Motto: ' + towns[i].motto;
+        myPara2.textContent = 'Year Founded: ' + towns[i].yearFounded;
+        myPara3.textContent = 'Current Population:' + towns[i].currentPopulation;
+        myPara4.textContent = 'Annual Rainfall:';
+   
+        myArticle.appendChild(myH2);
+        myArticle.appendChild(myPara1);
+        myArticle.appendChild(myPara2);
+        myArticle.appendChild(myPara3);
+        myArticle.appendChild(myPara4);
+
+        section.appendChild(myArticle);
+    }
+
+}
