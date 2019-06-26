6bed9777bd26e6601a0280b35ddf3829f2879c5b
Pythaules <pythaules@gmail.com>
2019-06-26 18:53:02 -0400

    L10 First

---
+++ Assignments/Lesson-10/js/scripts.js
@@ -0,0 +1,9 @@
+const weatherObject = new XMLHttpRequest();
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=23452,us&appid=338ef3b2197ac9e5d641b0bd773526c3", true);
+weatherObject.send();
+
+
+
+weatherObject.onload = function () {
+let weatherInfo = new JSON.parse(weatherObject.responceText);
+console.log(weatherInfo);
