12f15b9071f72ece1ee68128f36be08f76a4af77
Pythaules <pythaules@gmail.com>
2019-06-26 20:34:03 -0400

    L10 icon path

--- Assignments/Lesson-10/js/scripts.js
+++ Assignments/Lesson-10/js/scripts.js
@@ -13,7 +13,6 @@ weatherObject.onload = function () {
     document.getElementById('windSpeed').innerHTML = weatherInfo.wind.speed;
 
     const iconcode = weatherInfo.weather[0].icon;
-    console.log(iconcode);
-
-    //const icon_path = "//openweathermap.org/img/w/" 
+    const icon_path = "//openweathermap.org/img/w/" + iconcode + ".png";
+    console.log(icon_path);
 }
