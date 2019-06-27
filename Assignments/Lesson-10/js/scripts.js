f8466f2a4b676fba16aaf29061a1f56883a31d66
Pythaules <pythaules@gmail.com>
2019-06-26 20:54:10 -0400

    L10 cityID

--- Assignments/Lesson-10/js/scripts.js
+++ Assignments/Lesson-10/js/scripts.js
@@ -1,5 +1,5 @@
 const weatherObject = new XMLHttpRequest();
-weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?zip=23452,us&appid=338ef3b2197ac9e5d641b0bd773526c3&units=imperial", true);
+weatherObject.open("GET","//api.openweathermap.org/data/2.5/weather?id=5061036&appid=338ef3b2197ac9e5d641b0bd773526c3&units=imperial", true);
 weatherObject.send();
 
 
