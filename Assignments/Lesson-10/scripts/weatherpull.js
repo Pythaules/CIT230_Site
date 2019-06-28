4e74618960f12a3ff5c335b01f201a237c51b37d
Pythaules <pythaules@gmail.com>
2019-06-27 20:44:00 -0400

    L10 Current Temp

--- Assignments/Lesson-10/scripts/weatherpull.js
+++ Assignments/Lesson-10/scripts/weatherpull.js
@@ -15,6 +15,17 @@ forecastPull.send();
 weatherPull.onload = function() {
     let weatherData = JSON.parse(weatherPull.responseText);
     console.log(weatherData);
+    cTemp = weatherData.main.temp;
+    cSpeed = weatherData.wind.speed;
+    wChill = (35.74 + 0.6215 * cTemp - 35.75 * Math.pow(cSpeed, 0.16) + 0.4275 * cTemp * Math.pow(cSpeed, 0.16));
+
+    document.getElementById('currentWeather').innerHTML = weatherData.weather[0].main;
+    document.getElementById('currentHigh').innerHTML = weatherData.main.temp_max;
+    document.getElementById('currentWindChill').innerHTML = wChill
+    document.getElementById('currentHumidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('currentWindSpeed').innerHTML = weatherData.wind.speed;
+
+
 }
 
 forecastPull.onload = function() {
