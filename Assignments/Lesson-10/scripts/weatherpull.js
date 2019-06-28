20e8e1a45a3443bd679699193244b3490787536e
Pythaules <pythaules@gmail.com>
2019-06-28 19:07:47 -0400

    l

--- Assignments/Lesson-10/scripts/weatherpull.js
+++ Assignments/Lesson-10/scripts/weatherpull.js
@@ -29,11 +29,12 @@ forecastPull.send();
 
 forecastPull.onload = function() {
     let forecastData = JSON.parse(forecastPull.responseText);
+    let lists = [];
     console.log(forecastData);
     for (l = 0; l < forecastData.list.length; l++) {
         if (forecastData.list[l].dt_txt.includes("18:00:00"))
         console.log(forecastData.list[l]);
-        //const lists = forecastData.list[l];
+        lists = lists.push(forecastData.list[l]);
 
     }
 }
