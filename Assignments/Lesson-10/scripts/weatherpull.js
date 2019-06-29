aee541100e1c7b7f198e5aa680617fa3c623c96c
Pythaules <pythaules@gmail.com>
2019-06-29 14:33:28 -0400

    grid8

--- Assignments/Lesson-10/scripts/weatherpull.js
+++ Assignments/Lesson-10/scripts/weatherpull.js
@@ -43,12 +43,13 @@ function populateLists(jsonObj) {
     var myPara1 = document.createElement('p');
     var myPara2 = document.createElement('p');
 
+
     myPara1.textContent = lists.dt_txt.substr(5, 5);
     myPara2.textContent = lists.main.temp;
 
+
     myDiv.appendChild(myPara1);
     myDiv.appendChild(myPara2);
-    myPara2.appendChild('&#8457;')
 
     document.getElementById("forecast").appendChild(myDiv);
 }
