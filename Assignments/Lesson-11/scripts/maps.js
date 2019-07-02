62aa0ba31aacae26a564d7318dcf2b4d4f3fcdbe
Pythaules <pythaules@gmail.com>
2019-07-02 14:26:56 -0400

    L11 maps trial

--- Assignments/Lesson-11/scripts/maps.js
+++ Assignments/Lesson-11/scripts/maps.js
@@ -1,5 +1,6 @@
+var map;
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 42.0963, lng: -111.8766},
         zoom: 8
     });
