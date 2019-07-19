ab562322ed882b6ef3990d89ae935f8db4eb5a72
Pythaules <pythaules@gmail.com>
2019-07-19 19:57:24 -0400

    Maps

--- Assignments/Final/scripts/maps.js
+++ Assignments/Final/scripts/maps.js
@@ -1,9 +1,11 @@
 var map;
-if (window.location.pathname.includes("preston")) {
+if (window.location.pathname.includes("DC")) {
     var posIt = {center: {lat: 42.0963, lng: -111.8766}, zoom: 13};
-} else if (window.location.pathname.includes("fishhaven")) {
+} else if (window.location.pathname.includes("PA")) {
     var posIt = {center: {lat: 42.0380, lng: -111.4004}, zoom: 13};
-} else if (window.location.pathname.includes("sodasprings")) {
+} else if (window.location.pathname.includes("NC")) {
+    var posIt = {center: {lat: 42.6614, lng: -111.6062}, zoom: 13};
+} else if (window.location.pathname.includes("SC")) {
     var posIt = {center: {lat: 42.6614, lng: -111.6062}, zoom: 13};
 } else {
     var posIt = {center: {lat: 42.3213, lng: -111.5419}, zoom: 9};
