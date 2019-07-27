ee0c1adeca1449c37a988d8a79a16e6e4c6e7d53
Pythaules <pythaules@gmail.com>
2019-07-26 23:10:50 -0400

    Weather

---
+++ Assignments/Final/scripts/paWeather.js
@@ -0,0 +1,32 @@
+const weatherPull = new XMLHttpRequest();
+apiURL = "//api.openweathermap.org/data/2.5/weather?";
+var apiID = "id=4560349"
+
+apiKEY = "&appid=338ef3b2197ac9e5d641b0bd773526c3";
+weatherPull.open("GET",apiURL + apiID + apiKEY + "&units=imperial", true);
+weatherPull.send();
+
+weatherPull.onload = function() {
+    let weatherData = JSON.parse(weatherPull.responseText);
+    console.log(weatherData);
+    cTemp = weatherData.main.temp;
+    cSpeed = weatherData.wind.speed;
+    wChill = Math.round(35.74 + 0.6215 * cTemp - 35.75 * Math.pow(cSpeed, 0.16) + 0.4275 * cTemp * Math.pow(cSpeed, 0.16));
+    windDeg = weatherData.wind.deg;
+
+    document.getElementById('paWeather').innerHTML = weatherData.weather[0].main;
+    document.getElementById('paHigh').innerHTML = weatherData.main.temp_max;
+    document.getElementById('paChill').innerHTML = wChill
+    document.getElementById('paHumidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('paWindspeed').innerHTML = weatherData.wind.speed;
+    document.getElementById('paWinddir').innerHTML = degToCompass(windDeg);
+}
+
+function degToCompass(num) { 
+    while( num < 0 ) num += 360 ;
+    while( num >= 360 ) num -= 360 ; 
+    val= Math.round( (num -11.25 ) / 22.5 ) ;
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", 
+          "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"] ;
+    return arr[ Math.abs(val) ] ;
+}
---
+++ Assignments/Final/scripts/scWeather.js
@@ -0,0 +1,32 @@
+const weatherPull = new XMLHttpRequest();
+apiURL = "//api.openweathermap.org/data/2.5/weather?";
+var apiID = "id=4575352"
+
+apiKEY = "&appid=338ef3b2197ac9e5d641b0bd773526c3";
+weatherPull.open("GET",apiURL + apiID + apiKEY + "&units=imperial", true);
+weatherPull.send();
+
+weatherPull.onload = function() {
+    let weatherData = JSON.parse(weatherPull.responseText);
+    console.log(weatherData);
+    cTemp = weatherData.main.temp;
+    cSpeed = weatherData.wind.speed;
+    wChill = Math.round(35.74 + 0.6215 * cTemp - 35.75 * Math.pow(cSpeed, 0.16) + 0.4275 * cTemp * Math.pow(cSpeed, 0.16));
+    windDeg = weatherData.wind.deg;
+
+    document.getElementById('scWeather').innerHTML = weatherData.weather[0].main;
+    document.getElementById('scHigh').innerHTML = weatherData.main.temp_max;
+    document.getElementById('scChill').innerHTML = wChill
+    document.getElementById('scHumidity').innerHTML = weatherData.main.humidity;
+    document.getElementById('scWindspeed').innerHTML = weatherData.wind.speed;
+    document.getElementById('scWinddir').innerHTML = degToCompass(windDeg);
+}
+
+function degToCompass(num) { 
+    while( num < 0 ) num += 360 ;
+    while( num >= 360 ) num -= 360 ; 
+    val= Math.round( (num -11.25 ) / 22.5 ) ;
+    arr=["N","NNE","NE","ENE","E","ESE", "SE", 
+          "SSE","S","SSW","SW","WSW","W","WNW","NW","NNW"] ;
+    return arr[ Math.abs(val) ] ;
+}
